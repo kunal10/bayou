ecf9d1177da4923a57228d5a05e6d1403d9c23ca
Ashwini Venkatesh <asvenk@TACC-SpareMBP15.local>
2015-11-22 19:43:33 -0600

    Session Manager dummy

--- src/ut/distcomp/bayou/SessionManager.java
+++ src/ut/distcomp/bayou/SessionManager.java
@@ -1,13 +1,32 @@
 package ut.distcomp.bayou;
 
+import java.util.HashMap;
+
 import ut.distcomp.bayou.Utils.TransactionType;
 
 public class SessionManager {
 
+	
 	public String ExecuteTransaction(TransactionType delete, String songName,
 			String string) {
-		
+		/*
+		 * if(transaction type = read)
+		 * 		call read gurantees
+		 * else
+		 * 		call write gurantees 
+		 */
 		return null;
 	}
+	
+	private boolean ReadGuarantee(){
+		
+	}
+	
+	private boolean WriteGuarantee(){
+		
+	}
+	
+	// private HashMap<WriteID, Integer> readSet;
+	// private HashMap<WriteID, Integer> writeSet;
 
 }
