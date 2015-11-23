f85055b14ec81f1f82e9aaa13004f01b16798704
Ashwini Venkatesh <asvenk@TACC-SpareMBP15.local>
2015-11-23 15:41:06 -0600

    Server receive switch case

--- src/ut/distcomp/bayou/Server.java
+++ src/ut/distcomp/bayou/Server.java
@@ -3,6 +3,7 @@ package ut.distcomp.bayou;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Logger;
 
 import ut.distcomp.framework.Config;
 import ut.distcomp.framework.NetController;
@@ -16,22 +17,24 @@ public class Server implements NetworkNodes {
 				new Config(serverPid, "LogServer" + serverPid), queue);
 		this.dataStore = new DataStore();
 		this.writeLog = new WriteLog();
-		// This will be set 
+		// This will be set
 		this.serverId = null;
 		this.isPrimary = false;
 		this.csn = -1;
 		this.versionVector = new HashMap<>();
+		this.pause = true;
+		this.logger = nc.getConfig().logger;
 	}
 
-	public void joinServer(){
+	public void joinServer() {
 		// Make nc connections to all other existing servers
 		// Run creation algo and get a server ID
 	}
-	
-	public void retireServer(){
+
+	public void retireServer() {
 		// Run retirement protocol
 	}
-	
+
 	@Override
 	public void breakConnection(int i) {
 		nc.breakOutgoingConnection(i);
@@ -45,6 +48,58 @@ public class Server implements NetworkNodes {
 		}
 	}
 
+	// TODO: On creation when should other messages start coming
+	public void receiveMessages() {
+		// TODO: Will blocking queue be a problem for pause
+		while (pause) {
+			Message m = null;
+			try {
+				m = queue.take();
+			} catch (InterruptedException e) {
+				logger.info("Interrupted while waiting for message");
+			}
+			switch (m.getMsgType()) {
+			case ANTI_ENTROPY:
+				startAntiEntropy(m.getSrc());
+				break;
+			case CREATE_REQ:
+				creationWrite(m.getSrc());
+				break;
+			case CREATE_RES:
+				setServerId(m);
+				break;
+			case READ:
+				// TODO: Read from write log
+				break;
+			case RETIRE:
+				break;
+			case STATE_REQ:
+				break;
+			case WRITE:
+				break;
+			default:
+				logger.severe("Shouldn't have received " + m.toString());
+				break;
+			}
+		}
+	}
+
+	private void setServerId(Message m) {
+		serverId = new ServerId(m.getWriteId().getAcceptstamp(),
+				m.getWriteId().getServerId());
+	}
+
+	private void creationWrite(int src) {
+		// TODO Auto-generated method stub
+		// TODO Send a create_res to the source
+
+	}
+
+	private void startAntiEntropy(int src) {
+		// TODO Auto-generated method stub
+
+	}
+
 	private final int serverPid;
 	private final NetController nc;
 	private final LinkedBlockingQueue<Message> queue;
@@ -55,5 +110,6 @@ public class Server implements NetworkNodes {
 	private boolean isPrimary;
 	private int csn;
 	private HashMap<ServerId, Integer> versionVector;
-	
+	private boolean pause;
+	private Logger logger;
 }
